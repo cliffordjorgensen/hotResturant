3a7baaca9cd77d44964117b2fce720ae0a9a6fb5
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-06-27 14:39:52 -0700

    reservations

--- assets/app.js
+++ assets/app.js
@@ -7,18 +7,20 @@ var PORT = 1337;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var reservations = [{
-    routeName: "first customer",
-    name: "customer name",
-    phoneNum: "custumer phone number",
-    email: "customer email",
-    id: "reservation number"
-
-}];
+// var reservations = [{
+//     routeName: "reservations",
+//     name: "customer name",
+//     phoneNum: "custumer phone number",
+//     email: "customer email",
+//     id: "reservation number"
+// }];
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'tblView.html'))
+    res.sendFile(__dirname, 'tblView.html')
 })
+app.get("/reservations", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
